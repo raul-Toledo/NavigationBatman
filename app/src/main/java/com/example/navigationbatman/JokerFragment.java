42f99775bd8d05857e6e6f1ba0af2ab4e4da2219
Raul Toledo <ratp82@gmail.com>
2019-05-13 08:58:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/navigationbatman/JokerFragment.java
@@ -0,0 +1,24 @@
+package com.example.navigationbatman;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class JokerFragment extends Fragment {
+
+    public JokerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_joker, container, false);
+    }
+}
